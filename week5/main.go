b620ae049e7dad11251f2b06745c15c89f36bfd6
etkim <ket0825@gmail.com>
2024-11-11 19:13:05 +0900

    week5: seperate Server struct and mux handler in Run functions.

--- week5/main.go
+++ week5/main.go
@@ -5,18 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 	"week5/config"
-
-	"golang.org/x/sync/errgroup"
 )
 
-//	func main() {
-//		if err := run(context.Background()); err != nil {
-//			log.Printf("Failed to run: %v", err)
-//		}
-//	}
 func main() {
 	if err := run(context.Background()); err != nil {
 		log.Printf("Failed to run: %v", err)
@@ -25,36 +17,18 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	// Interrupt signal 신호 처리를 위한 context 생성
 	cfg, err := config.New()
 	if err != nil {
-		return nil
+		return err
 	}
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
-		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
+		return fmt.Errorf("failed to listen port %v", err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
-	log.Printf("start with: %v", url)
-
-	s := &http.Server{
-		// Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("Failed to close %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("Failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	log.Printf("start with %s", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
