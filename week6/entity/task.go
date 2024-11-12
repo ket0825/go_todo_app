e461ef0865df74e8e3ec6db0864c3ec4cdd2b3f8
etkim <ket0825@gmail.com>
2024-11-12 21:10:51 +0900

    week6: add endpoint with chi, validate with validator, add List handler, add test with struct and map compare

---
+++ week6/entity/task.go
@@ -0,0 +1,23 @@
+package entity
+
+import (
+	"time"
+)
+
+type TaskID int64
+type TaskStatus string
+
+const (
+	TaskStatusTodo  TaskStatus = "todo"
+	TaskStatusDoing TaskStatus = "done"
+	TaskStatusDone  TaskStatus = "done"
+)
+
+type Task struct {
+	ID      TaskID     `json:"id"` // Tag: json에서 id를 key로 둠.
+	Title   string     `json:"title"`
+	Status  TaskStatus `json:"status"`
+	Created time.Time  `json:"created"`
+}
+
+type Tasks []*Task
