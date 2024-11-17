70c1910e2c3f4f8b1119ea09326e2f8d1f0b2524
etkim <ket0825@gmail.com>
2024-11-18 03:15:51 +0900

    fix: Dropped role column

--- week6/_tools/mysql/schema.sql
+++ week6/_tools/mysql/schema.sql
@@ -13,7 +13,6 @@ CREATE TABLE `task` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '태스크 식별자',
     `title` VARCHAR(128) NOT NULL COMMENT '테스크 타이틀',
     `status` VARCHAR(80) NOT NULL COMMENT '테스크 상태',
-    `role`  VARCHAR(80) NOT NULL COMMENT '역할',
     `created` DATETIME(6) NOT NULL COMMENT '레코드 작성 시간',
     `modified` DATETIME(6) NOT NULL COMMENT '레코드 수정 시간',
     PRIMARY KEY (`id`)
