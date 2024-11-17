eb9deabc72fe89388f2432b2c62d247f2cf00198
etkim <ket0825@gmail.com>
2024-11-18 03:14:26 +0900

    develop: interface로 type별 메소드를 정의함

---
+++ week6/store/repository.go
@@ -0,0 +1,72 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/ket0825/go_todo_app/clock"
+	"github.com/ket0825/go_todo_app/config"
+)
+
+func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
+	// sqlx.Connect를 사용하면 내부에서 ping한다
+	db, err := sql.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",
+			cfg.DBUser,
+			cfg.DBPassword,
+			cfg.DBHost,
+			cfg.DBPort,
+			cfg.DBName,
+		),
+	)
+	if err != nil {
+		return nil, func() {}, err
+	}
+	// Open은 실제 접속을 테스트하지는 않음.
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		return nil, func() { _ = db.Close() }, err
+	}
+	xdb := sqlx.NewDb(db, "mysql")
+	return xdb, func() { _ = db.Close() }, nil
+}
+
+// 여러 테이블을 하나의 type method로 관리하기 위해 Repository를 사용
+// 의존성 주입을 사용하는 경우, 하나의 type으로 통일하여 쉽게 처리 가능.
+type Repository struct {
+	Clocker clock.Clocker
+}
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	// 인터페이스와 실제 타입의 시그니처가 일치하는 지 확인하는 코드
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
